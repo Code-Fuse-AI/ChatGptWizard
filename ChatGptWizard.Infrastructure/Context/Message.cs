5df65547189ba49008a6e2f66622bdd16d8acb95
AMTdev Alien <craig@amtdev.com>
2023-04-23 21:39:24 -0400

    removed ef core service and repo implementation

--- ChatGptWizard.Infrastructure/Context/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChatGptWizard.Infrastructure.Context
-{
-    public class Message
-    {
-        public int Id { get; set; }
-        public string Text { get; set; }
-        public DateTime CreateAt { get; set; }
-    }
-}
